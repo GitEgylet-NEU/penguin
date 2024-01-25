1f6123d817dc11557d306c7b8cf31d160c864f49
zuipzuip <momarci0631@gmail.com>
2024-01-25 23:25:13 +0100

    obstacles, random generálás

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -5,26 +5,53 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
+    public GameObject[] levelChunkDatas;
+    public float[] lenght;
     public GameObject Section;
-    int z = 25;
+    float z = 25;
+    int rand = -1;
+    int lastrand = -1;
 
     public void Start()
     {
         // elsõ blokkok generálása
         Instantiate(Section, new Vector3(0, -0.1f, 5), Quaternion.identity);
         Instantiate(Section, new Vector3(0, -0.1f, 15), Quaternion.identity);
+
+        //hosszúság bekérése
+        lenght = new float[levelChunkDatas.Length];
+            for (int i = 0; i < levelChunkDatas.Length; i++)
+            {
+                lenght[i] = levelChunkDatas[i].transform.localScale.z;
+            }
+        
     }
 
-   
     public void Update()
     {
-        // generálás
+        // endless generálás
         TeamManager tam = FindObjectOfType<TeamManager>();
         if (TeamManager.instance.penguins[0].gameObject.transform.position.z > z - 20)
         {
+            int id = NotSame();
             Debug.Log("generate");
-            Instantiate(Section, new Vector3(0, -0.1f, z), Quaternion.identity);
-            z += 10;
+            Instantiate(levelChunkDatas[id], new Vector3(0, -0.1f, z), Quaternion.identity);
+            z += lenght[id];
         }
+
+
+
+    }
+
+    //különbözõ generálásának a biztosítása
+    public int NotSame() {
+
+        while (lastrand == rand)
+        {
+        rand = UnityEngine.Random.Range(0, 4);
+        }
+        lastrand = rand;
+        
+        return rand;
     }
 }
