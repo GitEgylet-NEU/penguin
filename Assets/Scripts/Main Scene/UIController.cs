63480bb76879ceec2a8eab5562028fcd0f47ad91
Hunor Csuhai <team3csh@gmail.com>
2026-06-12 12:16:22 +0200

    added missing localization in main menu
    XP level and upgrade point indicators now work properly in all locales.

--- Assets/Scripts/Main Scene/UIController.cs
+++ Assets/Scripts/Main Scene/UIController.cs
@@ -7,158 +7,141 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-	public static UIController instance;
+    public static UIController instance;
 
-	public bool gameOn = false;
-	public bool move = false;
+    public bool gameOn = false;
+    public bool move = false;
 
-	[Header("Main Menu")]
-	public GameObject mainMenu;
-	public GameObject tapToPlayText;
-	public GameObject manualObject, creditsObject;
-	[Space]
-	public LocalizedString levelString, upgradePointString;
-	public TextMeshProUGUI levelText, upgradePointText;
-	[Space]
-	public GameObject popupObject;
-	public TextMeshProUGUI popupTitle, popupText;
-	public Button popupButton;
+    [Header("Main Menu")]
+    public GameObject mainMenu;
+    public GameObject tapToPlayText;
+    public GameObject manualObject, creditsObject;
+    [Space]
+    public LocalizeStringEvent xpLevelLocalizer;
+    public LocalizeStringEvent upgradePointsLocalizer;
+    public TextMeshProUGUI levelText, upgradePointText;
+    [Space]
+    public GameObject popupObject;
+    public TextMeshProUGUI popupTitle, popupText;
+    public Button popupButton;
 
-	[Header("Run")]
-	public ProgressBar runProgressBar;
+    [Header("Run")]
+    public ProgressBar runProgressBar;
 
-	[Header("Game Over")]
-	public GameObject gameOverObject;
-	public TextMeshProUGUI gameOverText;
-	public LocalizeStringEvent restartButtonLocalizer;
-	[SerializeField] LocalizedString hoorayString, restartString;
-	public GameObject lostXpWindow;
-	public TextMeshProUGUI lostXpText;
-	public GameObject wonXpWindow;
-	public TextMeshProUGUI wonXpText;
+    [Header("Game Over")]
+    public GameObject gameOverObject;
+    public TextMeshProUGUI gameOverText;
+    public LocalizeStringEvent restartButtonLocalizer;
+    [SerializeField] LocalizedString hoorayString, restartString;
+    public GameObject lostXpWindow;
+    public TextMeshProUGUI lostXpText;
+    public GameObject wonXpWindow;
+    public TextMeshProUGUI wonXpText;
 
-	void Awake()
-	{
-		instance = this;
-	}
+    void Awake()
+    {
+        instance = this;
+    }
 
-	private void OnEnable()
-	{
-		levelString.Arguments = new object[1];
-		//levelString.StringChanged += UpdateLevelText;
+    private void OnEnable()
+    {
+        xpLevelLocalizer.StringReference.Arguments = new object[1];
+        upgradePointsLocalizer.StringReference.Arguments = new object[1];
+    }
 
-		upgradePointString.Arguments = new object[1];
-		//upgradePointString.StringChanged += UpdateUpgradePointText;
-	}
-	private void OnDisable()
-	{
-		levelString.StringChanged -= UpdateLevelText;
-		upgradePointString.StringChanged -= UpdateUpgradePointText;
-	}
+    private void Start()
+    {
+        gameOverObject.SetActive(false);
+        mainMenu.SetActive(true);
 
-	private void Start()
-	{
-		gameOverObject.SetActive(false);
-		mainMenu.SetActive(true);
+        runProgressBar.min = 0;
 
-		runProgressBar.min = 0;
+        xpLevelLocalizer.StringReference.Arguments[0] = SaveManager.instance.progressData.level + 1;
+        xpLevelLocalizer.RefreshString();
+        upgradePointsLocalizer.StringReference.Arguments[0] = SaveManager.instance.progressData.upgradePoints;
+        upgradePointsLocalizer.RefreshString();
+    }
 
-		levelString.Arguments[0] = SaveManager.instance.progressData.level + 1;
-		levelString.RefreshString();
-		upgradePointString.Arguments[0] = SaveManager.instance.progressData.upgradePoints;
-		upgradePointString.RefreshString();
-	}
+    public void OnLayoutClicked()
+    {
+        SceneManager.LoadScene("LayoutPlanning");
+    }
+    public void OnPlayClicked()
+    {
+        move = true;
+        gameOn = true;
+        mainMenu.SetActive(false);
+        TeamManager.instance.StartGame();
+    }
+    public void OnManualClicked()
+    {
+        ActivateLayer(manualObject, creditsObject, tapToPlayText);
+    }
+    public void OnCreditsClicked()
+    {
+        ActivateLayer(creditsObject, manualObject, tapToPlayText);
+    }
 
-	void UpdateLevelText(string value)
-	{
-		Debug.Log("update level to " + value);
-		levelText.text = value;
-	}
-	void UpdateUpgradePointText(string value)
-	{
-		Debug.Log("update points to " + value);
-		upgradePointText.text = value;
-	}
+    /// <summary>Activates a UI layer and deactivates others.</summary>
+    /// <param name="visual"><see cref="GameObject"/> to enable.</param>
+    /// <param name="antivisual"><see cref="GameObject"/>s to disable.</param>
+    public void ActivateLayer(GameObject visual, params GameObject[] antivisual)
+    {
+        if (visual.activeInHierarchy)
+        {
+            visual.SetActive(false);
+            tapToPlayText.SetActive(true);
+            mainMenu.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            tapToPlayText.SetActive(false);
+            visual.SetActive(true);
+            foreach (var av in antivisual)
+            {
+                av.SetActive(false);
+            }
+            mainMenu.GetComponent<Button>().interactable = false;
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void EndGame(bool won, float xp)
+    {
+        if (won)
+        {
+            gameOverText.text = "YOU won";
+            restartButtonLocalizer.StringReference = hoorayString;
+            restartButtonLocalizer.RefreshString();
+            if (xp > 0f)
+            {
+                wonXpText.text = $"{xp} XP";
+                wonXpWindow.SetActive(true);
+            }
+        }
+        else
+        {
+            gameOverText.text = "YOU lost";
+            restartButtonLocalizer.StringReference = restartString;
+            restartButtonLocalizer.RefreshString();
+            if (xp > 0f)
+            {
+                lostXpText.text = $"<s>{xp} XP</s>   ►   <b>{xp / 2f} XP";
+                lostXpWindow.SetActive(true);
+            }
+        }
 
-	public void OnLayoutClicked()
-	{
-		SceneManager.LoadScene("LayoutPlanning");
-	}
-	public void OnPlayClicked()
-	{
-		move = true;
-		gameOn = true;
-		mainMenu.SetActive(false);
-		TeamManager.instance.StartGame();
-	}
-	public void OnManualClicked()
-	{
-		ActivateLayer(manualObject, creditsObject, tapToPlayText);
-	}
-	public void OnCreditsClicked()
-	{
-		ActivateLayer(creditsObject, manualObject, tapToPlayText);
-	}
+        gameOverObject.SetActive(true);
+    }
 
-	/// <param name="visual"><see cref="GameObject"/> to enable</param>
-	/// <param name="antivisual"><see cref="GameObject"/>s to disable</param>
-	public void ActivateLayer(GameObject visual, params GameObject[] antivisual)
-	{
-		if (visual.activeInHierarchy)
-		{
-			visual.SetActive(false);
-			tapToPlayText.SetActive(true);
-			mainMenu.GetComponent<Button>().interactable = true;
-		}
-		else
-		{
-			tapToPlayText.SetActive(false);
-			visual.SetActive(true);
-			foreach (var av in antivisual)
-			{
-				av.SetActive(false);
-			}
-			mainMenu.GetComponent<Button>().interactable = false;
-		}
-	}
-	public void RestartGame()
-	{
-		SceneManager.LoadScene("SampleScene");
-	}
-	public void EndGame(bool won, float xp)
-	{
-		if (won)
-		{
-			gameOverText.text = "YOU won";
-			restartButtonLocalizer.StringReference = hoorayString;
-			restartButtonLocalizer.RefreshString();
-			if (xp > 0f)
-			{
-				wonXpText.text = $"{xp} XP";
-				wonXpWindow.SetActive(true);
-			}
-		}
-		else
-		{
-			gameOverText.text = "YOU lost";
-			restartButtonLocalizer.StringReference = restartString;
-			restartButtonLocalizer.RefreshString();
-			if (xp > 0f)
-			{
-				lostXpText.text = $"<s>{xp} XP</s>   ►   <b>{xp / 2f} XP";
-				lostXpWindow.SetActive(true);
-			}
-		}
-
-		gameOverObject.SetActive(true);
-	}
-
-	public void SetPopup(string titleText, string text)
-	{
-		//Debug.Log($"setPopup: {titleText}, {text}");
-		popupTitle.text = titleText;
-		popupText.text = text;
-		popupButton.onClick.AddListener(() => ActivateLayer(tapToPlayText, popupObject));
-		ActivateLayer(popupObject, manualObject, creditsObject);
-	}
+    public void SetPopup(string titleText, string text)
+    {
+        //Debug.Log($"setPopup: {titleText}, {text}");
+        popupTitle.text = titleText;
+        popupText.text = text;
+        popupButton.onClick.AddListener(() => ActivateLayer(tapToPlayText, popupObject));
+        ActivateLayer(popupObject, manualObject, creditsObject);
+    }
 }
